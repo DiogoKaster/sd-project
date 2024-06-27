4e1237b52656b94b71439d7ffc0544281e6c31ae
DiogoKaster <diogokaster@gmail.com>
2024-06-27 20:42:50 -0300

    feat: final commit

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,4 +1,5 @@
 package server;
+
 import java.io.*;
 import java.net.*;
 import helpers.Json;
@@ -6,26 +7,30 @@ import records.*;
 import server.routes.Routes;
 import server.views.UserConnected;
 
-public class Server extends Thread{
+public class Server extends Thread {
     private final Socket client;
-    public static void main(String[] args)   {
+    private static final UserConnected userConnected = new UserConnected();
+
+    public static void main(String[] args) {
         try {
-            Server.startConnection();
+            startConnection();
         } catch (IOException e) {
             System.exit(1);
         }
     }
-    private Server(Socket clientSock){
+
+    private Server(Socket clientSock) {
         client = clientSock;
         start();
     }
+
     private static void startConnection() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Which port should be used?");
         int serverPort = Integer.parseInt(br.readLine());
-        try(ServerSocket server = new ServerSocket(serverPort, 0)){
+        try (ServerSocket server = new ServerSocket(serverPort, 0)) {
             System.out.println("Connection Socket Created");
-            while(true){
+            while (true) {
                 try {
                     System.out.println("Waiting for Connection");
                     new Server(server.accept());
@@ -34,26 +39,27 @@ public class Server extends Thread{
                     System.exit(1);
                 }
             }
-        }
-        catch (IOException e){
-            System.err.println("Could not listen on port: "+serverPort);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + serverPort);
             System.exit(1);
         }
     }
 
     @Override
-    public void run(){
+    public void run() {
         System.out.println("New thread started");
-        System.out.println(client.getInetAddress().getHostAddress());
+        String clientIP = client.getInetAddress().getHostAddress();
+        System.out.println(clientIP);
 
-        try(
-            client;
-            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))
-        ){
+        userConnected.addClientIP(clientIP);
+
+        try (
+                client;
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))
+        ) {
             String request;
-            while((request = in.readLine()) != null){
-
+            while ((request = in.readLine()) != null) {
                 Json json = Json.getInstance();
                 System.out.println("[LOG]: RECEIVING REQUEST: " + request);
                 Request<?> clientRequest = json.fromJson(request, Request.class);
@@ -63,9 +69,11 @@ public class Server extends Thread{
                 String jsonResponse = json.toJson(response);
                 System.out.println("[LOG]: SENDING RESPONSE: " + jsonResponse);
                 out.println(jsonResponse);
+
+                userConnected.addRequest(clientIP, request);
+                userConnected.addResponse(clientIP, jsonResponse);
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
